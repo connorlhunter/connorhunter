135c04b1d4c839afe3ff835bbd9c51c2e6860e96
Connor Hunter <connh321@gmail.com>
2026-06-21 21:05:54 -0400

    modify amplify configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,18 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [tanstackStart(), tailwindcss(), react()],
+  plugins: [
+    tanstackStart({
+      spa: {
+        enabled: true,
+        prerender: {
+          outputPath: "/index.html",
+        },
+      },
+    }),
+    tailwindcss(),
+    react(),
+  ],
   resolve: {
     tsconfigPaths: true,
   },
